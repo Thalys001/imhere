98d2b75a9fb094ef5e9a4c9704d4a4b3a5f8b349
Thalys001 <thalyssouza708@gmail.com>
2023-10-06 22:32:50 -0300

    feat: creating add participant button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,12 @@
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log('adicionar');
+
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -11,11 +15,20 @@ export function Home() {
       <Text style={styles.eventDate}>
         Sexta, 04 de Novembro de 2022
       </Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
